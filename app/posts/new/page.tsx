91895fdce651140d0d6e5ae186e76f5cbabf1923
Murod Akhmedov <murod.london@gmail.com>
2024-09-27 15:13:27 +0100

    Handle form submission

--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -1,20 +1,35 @@
-
+'use client'
 import { Box, Button, Input, Textarea } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+
+interface FormData {
+  title: string
+  description: string
+}
 
 const NewPostView = () => {
+  const { push } = useRouter()
+  const { register, handleSubmit } = useForm<FormData>()
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/posts', data)
+        push('/posts')
+      })}
+    >
       <Box
         maxW={'md'}
         display={'flex'}
         flexDirection={'column'}
         gap={4}
       >
-        <Input placeholder='Basic usage' />
+        <Input placeholder='Title' {...register('title')} />
         <Textarea
           resize={'none'}
           rows={6}
-          placeholder='Basic usage' />
+          placeholder='Description' {...register('description')} />
         <Button
           type="submit"
           colorScheme='yellow'
