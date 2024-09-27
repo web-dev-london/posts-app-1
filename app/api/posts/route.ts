5895802e08de4c10e5825db82e68cc35765d9cef
Murod Akhmedov <murod.london@gmail.com>
2024-09-27 15:30:06 +0100

    Handling Errors

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,15 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod'
 
 const postSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1)
+  title: z.string().min(1, 'Title is required').max(255),
+  description: z.string().min(1, 'Description is required').max(255)
 })
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = postSchema.safeParse(body);
   if (!validation.success) {
-    return NextResponse.json({ error: validation.error }, { status: 400 });
+    return NextResponse.json({ error: validation.error.format() }, { status: 400 });
   }
 
   const newPost = await prisma.post.create({
--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { Box, Button, Input, Textarea } from '@chakra-ui/react'
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, Input, Textarea } from '@chakra-ui/react'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 interface FormData {
   title: string
@@ -11,36 +12,58 @@ interface FormData {
 
 const NewPostView = () => {
   const { push } = useRouter()
+  const [error, setError] = useState('');
   const { register, handleSubmit } = useForm<FormData>()
   return (
-    <form
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/posts', data)
-        push('/posts')
-      })}
-    >
+    <>
       <Box
         maxW={'md'}
-        display={'flex'}
-        flexDirection={'column'}
-        gap={4}
+        whiteSpace={'wrap'}
       >
-        <Input placeholder='Title' {...register('title')} />
-        <Textarea
-          resize={'none'}
-          rows={6}
-          placeholder='Description' {...register('description')} />
-        <Button
-          type="submit"
-          colorScheme='yellow'
-          flexGrow={'1'}
-          ml={'auto'}
-          display={'inline-block'}
+        {error &&
+          (<Alert
+            borderRadius={'5px'}
+            my={5}
+            status='error'>
+            <AlertIcon />
+            <AlertTitle>{error}</AlertTitle>
+            <AlertDescription>Something went wrong...</AlertDescription>
+          </Alert>)}
+        <form
+          onSubmit={handleSubmit(async (data) => {
+            try {
+              await axios.post('/api/posts', data)
+              push('/posts')
+            } catch (error) {
+              if (error instanceof Error) {
+                setError(error.message)
+              }
+            }
+          })}
         >
-          Submit
-        </Button>
+          <Box
+            display={'flex'}
+            flexDirection={'column'}
+            gap={4}
+          >
+            <Input placeholder='Title' {...register('title')} />
+            <Textarea
+              resize={'none'}
+              rows={6}
+              placeholder='Description' {...register('description')} />
+            <Button
+              type="submit"
+              colorScheme='yellow'
+              flexGrow={'1'}
+              ml={'auto'}
+              display={'inline-block'}
+            >
+              Submit
+            </Button>
+          </Box>
+        </form>
       </Box>
-    </form>
+    </>
   )
 }
 
