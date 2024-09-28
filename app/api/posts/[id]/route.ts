1a6011c96ad4035adf4851322286615f6e70e06c
Murod Akhmedov <murod.london@gmail.com>
2024-09-28 12:48:46 +0100

    Build an API for updating post

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,32 @@
+import prisma from "@/prisma/client";
+import postSchema from "@/schema/postSchema";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+  const body = await request.json();
+  const validation = postSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json({ error: validation.error.format() }, { status: 400 });
+  }
+
+  const post = await prisma.post.findUnique({
+    where: {
+      id: parseInt(params.id)
+    }
+  })
+  if (!post) {
+    return NextResponse.json({ error: "Post not found" }, { status: 404 });
+  }
+
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: parseInt(params.id)
+    },
+    data: {
+      title: body.title,
+      description: body.description
+    }
+  })
+
+  return NextResponse.json(updatedPost, { status: 200 });
+}
