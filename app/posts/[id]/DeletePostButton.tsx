f1322e2c3b26b5bd88ad618eab60034b165c8862
Murod Akhmedov <murod.london@gmail.com>
2024-09-29 19:43:46 +0100

    Show confirmation of dialog box

--- app/posts/[id]/DeletePostButton.tsx
+++ app/posts/[id]/DeletePostButton.tsx
@@ -1,24 +1,49 @@
-import { Button } from '@chakra-ui/react'
+'use client'
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, useDisclosure } from '@chakra-ui/react'
 // import Link from 'next/link'
 import React from 'react'
 import { FaDeleteLeft } from "react-icons/fa6";
 
 const DeletePostButton = ({ id }: { id: number }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef<HTMLButtonElement>(null)
   return (
     <>
       <Button
         colorScheme='red'
         display={'flex'}
         gap={2}
+        onClick={onOpen}
       >
         <FaDeleteLeft />
-        {/* <Link
-          href={`/posts/${id}/edit`}
-        > */}
         Delete Post
-        {/* </Link> */}
       </Button>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Delete Customer
+            </AlertDialogHeader>
 
+            <AlertDialogBody>
+              Are you sure? You can not undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme='red' onClick={onClose} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </>
   )
 }
