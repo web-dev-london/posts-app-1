2a6c3766fe98f9a651235fb3a1a0b67496fc63d1
Murod Akhmedov <murod.london@gmail.com>
2024-09-29 20:18:42 +0100

    Handle errors

--- app/posts/[id]/DeletePostButton.tsx
+++ app/posts/[id]/DeletePostButton.tsx
@@ -2,14 +2,27 @@
 import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, useDisclosure } from '@chakra-ui/react'
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-// import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { FaDeleteLeft } from "react-icons/fa6";
 
 const DeletePostButton = ({ id }: { id: number }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const cancelRef = React.useRef<HTMLButtonElement>(null)
   const { push, refresh } = useRouter();
+  const [isError, setIsError] = useState(false);
+
+  const handleDeleteClick = async () => {
+    try {
+      await axios.delete(`/api/posts/${id}`)
+      push('/posts/list')
+      refresh()
+    } catch (error) {
+      if (error instanceof Error) {
+        setIsError(true)
+      }
+    }
+  };
+
   return (
     <>
       <Button
@@ -25,11 +38,12 @@ const DeletePostButton = ({ id }: { id: number }) => {
         isOpen={isOpen}
         leastDestructiveRef={cancelRef}
         onClose={onClose}
+        isCentered
       >
         <AlertDialogOverlay>
           <AlertDialogContent>
             <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-              Delete Customer
+              Delete Post
             </AlertDialogHeader>
 
             <AlertDialogBody>
@@ -40,13 +54,39 @@ const DeletePostButton = ({ id }: { id: number }) => {
               <Button ref={cancelRef} onClick={onClose}>
                 Cancel
               </Button>
-              <Button colorScheme='red' onClick={async () => {
-                onClose()
-                await axios.delete(`/api/posts/${id}`)
-                push('/posts/list')
-                refresh()
-              }} ml={3}>
+              <Button
+                colorScheme='red'
+                onClick={handleDeleteClick}
+                ml={3}>
                 Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+
+
+      <AlertDialog
+        isOpen={isError}
+        isCentered
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              An expected error occurred
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Something went wrong
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={
+                () => setIsError(false)
+              }>
+                Close
               </Button>
             </AlertDialogFooter>
           </AlertDialogContent>
