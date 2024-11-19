0a5fdfd58a64d96427d4dd8927f9f04e7e13ea6d
Murod Akhmedov <murod.london@gmail.com>
2024-11-19 13:43:22 +0000

    Producton deploy

--- prisma/migrations/20240930163853_add_assigned_posts/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Post` ADD COLUMN `assignedToUserId` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Post` ADD CONSTRAINT `Post_assignedToUserId_fkey` FOREIGN KEY (`assignedToUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
